799a92da5f5453479e0e7a7b812a4626d301dc7d
fanyang <fanyang89@outlook.com>
2024-12-28 12:34:44 +0800

    Add RPC service

--- RaftNET/Services/RaftService.cs
+++ RaftNET/Services/RaftService.cs
@@ -1,7 +1,88 @@
+using System.Diagnostics;
+using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace RaftNET.Services;
 
-public class RaftService(ILogger<RaftService> logger) : Raft.RaftBase {
+public class RaftService(
+    ulong id,
+    ulong currentTerm,
+    ulong votedFor,
+    Log log,
+    ulong commitIdx,
+    ILogger<RaftService> logger
+) : Raft.RaftBase {
     private readonly ILogger<RaftService> _logger = logger;
+
+    private readonly FSM _fsm = new FSM(
+        id, currentTerm, votedFor, log, commitIdx,
+        new TrivialFailureDetector(),
+        new FSMConfig(),
+        new NullLogger<FSM>() // TODO: inject logger
+    );
+
+    private readonly string FromServerIdKey = "raftnet-from-id";
+
+    private ulong GetFromServerId(Metadata metadata) {
+        Debug.Assert(metadata.Any(x => x.Key == FromServerIdKey));
+        var entry = metadata.First(x => x.Key == FromServerIdKey);
+        return Convert.ToUInt64(entry.Value);
+    }
+
+    public override Task<Void> Vote(VoteRequest request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> RespondVote(VoteResponse request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> Append(AppendRequest request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> RespondAppend(AppendResponse request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> SendSnapshot(InstallSnapshot request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> RespondSendSnapshot(SnapshotResponse request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
+
+    public override Task<Void> TimeoutNow(TimeoutNowRequest request, ServerCallContext context) {
+        var from = GetFromServerId(context.RequestHeaders);
+        lock (_fsm) {
+            _fsm.Step(from, request);
+        }
+        return Task.FromResult(new Void());
+    }
 }
