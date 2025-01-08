af44a43de6942db7e08ac87e230f000241236726
fanyang <fanyang89@outlook.com>
2025-01-08 22:21:07 +0800

    Merge test setups

--- RaftNET.Tests/LogMatchingRuleTest.cs
+++ RaftNET.Tests/LogMatchingRuleTest.cs
@@ -19,23 +19,25 @@ public class LogMatchingRuleTest : FSMTestBase {
         fsm.GetOutput();
 
         fsm.Step(Id2, new VoteRequest { CurrentTerm = 9, LastLogIdx = 1001, LastLogTerm = 11 });
-        // Current term is too old - vote is not granted
-        var output = fsm.GetOutput();
-        Assert.That(output.Messages.Count, Is.Zero);
 
-        // Last stable index is too small - vote is not granted
-        Assert.That(RequestVote(fsm, 11, 999, 10).VoteGranted, Is.False);
-        // Last stable term is too small - vote is not granted
-        Assert.That(RequestVote(fsm, 12, 1002, 9).VoteGranted, Is.False);
-        // Last stable term and index are equal to the voter's - vote is granted
-        Assert.That(RequestVote(fsm, 13, 1000, 10).VoteGranted, Is.True);
-        // Last stable term is the same, index is greater to the voter's - vote is granted
-        Assert.That(RequestVote(fsm, 14, 1001, 10).VoteGranted, Is.True);
-        // Both term and index are greater than the voter's - vote is granted
-        Assert.That(RequestVote(fsm, 15, 1001, 11).VoteGranted, Is.True);
+        // The current term is too old - vote is not granted
+        var output = fsm.GetOutput();
+        Assert.Multiple(() => {
+            Assert.That(output.Messages, Is.Empty);
+            // The last stable index is too small - vote is not granted
+            Assert.That(RequestVote(fsm, 11, 999, 10).VoteGranted, Is.False);
+            // The last stable term is too small - vote is not granted
+            Assert.That(RequestVote(fsm, 12, 1002, 9).VoteGranted, Is.False);
+            // The last stable term and index are equal to the voter's - vote is granted
+            Assert.That(RequestVote(fsm, 13, 1000, 10).VoteGranted, Is.True);
+            // The last stable term is the same, index is greater to the voter's - vote is granted
+            Assert.That(RequestVote(fsm, 14, 1001, 10).VoteGranted, Is.True);
+            // Both term and index are greater than the voter's - vote is granted
+            Assert.That(RequestVote(fsm, 15, 1001, 11).VoteGranted, Is.True);
+        });
     }
 
-    private VoteResponse RequestVote(FSM fsm, ulong term, ulong lastLogIdx, ulong lastLogTerm) {
+    private static VoteResponse RequestVote(FSM fsm, ulong term, ulong lastLogIdx, ulong lastLogTerm) {
         fsm.Step(Id2, new VoteRequest { CurrentTerm = term, LastLogIdx = lastLogIdx, LastLogTerm = lastLogTerm });
         var output = fsm.GetOutput();
         return output.Messages.Last().Message.VoteResponse;
--- RaftNET.Tests/SetupTest.cs
+++ RaftNET.Tests/SetupTest.cs
@@ -1,14 +1,19 @@
 ﻿using System.Diagnostics;
+using System.Net;
 using NUnit.Framework.Diagnostics;
 
 namespace RaftNET.Tests;
 
 [SetUpFixture]
-public class SetupTrace {
+public class SetupTest {
     [OneTimeSetUp]
     public void Setup() {
+        // setup progress trace
         if (!Trace.Listeners.OfType<ProgressTraceListener>().Any()) {
             Trace.Listeners.Add(new ProgressTraceListener());
         }
+
+        // disable HTTP proxy
+        HttpClient.DefaultProxy = new WebProxy();
     }
 }
