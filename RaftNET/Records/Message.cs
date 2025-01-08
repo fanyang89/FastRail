ffb6db38476500afc046d015c8e93d8a8c43fef1
fanyang <fanyang89@outlook.com>
2025-01-08 22:47:31 +0800

    Add more asserts

--- RaftNET/Records/Message.cs
+++ RaftNET/Records/Message.cs
@@ -1,4 +1,5 @@
-﻿using OneOf;
+﻿using System.Diagnostics;
+using OneOf;
 
 namespace RaftNET.Records;
 
@@ -17,28 +18,80 @@ public class Message : OneOfBase<VoteRequest, VoteResponse, AppendRequest, Appen
     public Message(SnapshotResponse request) : base(request) {}
     public Message(TimeoutNowRequest request) : base(request) {}
 
-    public ulong CurrentTerm => Match(
-        x => x.CurrentTerm,
-        x => x.CurrentTerm,
-        x => x.CurrentTerm,
-        x => x.CurrentTerm,
-        x => x.CurrentTerm,
-        x => x.CurrentTerm,
-        x => x.CurrentTerm
-    );
+    public ulong CurrentTerm {
+        get {
+            return Match(
+                x => x.CurrentTerm,
+                x => x.CurrentTerm,
+                x => x.CurrentTerm,
+                x => x.CurrentTerm,
+                x => x.CurrentTerm,
+                x => x.CurrentTerm,
+                x => x.CurrentTerm
+            );
+        }
+    }
 
-    public VoteRequest VoteRequest => AsT0;
-    public VoteResponse VoteResponse => AsT1;
-    public AppendRequest AppendRequest => AsT2;
-    public AppendResponse AppendResponse => AsT3;
-    public InstallSnapshot InstallSnapshot => AsT4;
-    public SnapshotResponse SnapshotResponse => AsT5;
-    public TimeoutNowRequest TimeoutNowRequest => AsT6;
+    public VoteRequest VoteRequest {
+        get {
+            Debug.Assert(IsVoteRequest);
+            return AsT0;
+        }
+    }
+
+    public VoteResponse VoteResponse {
+        get {
+            Debug.Assert(IsVoteResponse);
+            return AsT1;
+        }
+    }
+
+    public AppendRequest AppendRequest {
+        get {
+            Debug.Assert(IsAppendRequest);
+            return AsT2;
+        }
+    }
+
+    public AppendResponse AppendResponse {
+        get {
+            Debug.Assert(IsAppendResponse);
+            return AsT3;
+        }
+    }
+
+    public InstallSnapshot InstallSnapshot {
+        get {
+            Debug.Assert(IsInstallSnapshot);
+            return AsT4;
+        }
+    }
+
+    public SnapshotResponse SnapshotResponse {
+        get {
+            Debug.Assert(IsSnapshotResponse);
+            return AsT5;
+        }
+    }
+
+    public TimeoutNowRequest TimeoutNowRequest {
+        get {
+            Debug.Assert(IsTimeoutNowRequest);
+            return AsT6;
+        }
+    }
+
     public bool IsVoteRequest => IsT0;
+
     public bool IsVoteResponse => IsT1;
+
     public bool IsAppendRequest => IsT2;
+
     public bool IsAppendResponse => IsT3;
+
     public bool IsInstallSnapshot => IsT4;
+
     public bool IsSnapshotResponse => IsT5;
+
     public bool IsTimeoutNowRequest => IsT6;
 }
