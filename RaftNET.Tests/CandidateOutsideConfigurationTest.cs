1a9cce1fe0e7c9dc6b34483bd0a2b008f3254f70
fanyang <fanyang89@outlook.com>
2025-01-17 15:19:28 +0800

    Add CandidateOutsideConfigurationTest

---
+++ RaftNET.Tests/CandidateOutsideConfigurationTest.cs
@@ -0,0 +1,35 @@
+using RaftNET.FailureDetectors;
+
+namespace RaftNET.Tests;
+
+public class CandidateOutsideConfigurationTest : FSMTestBase {
+    [Test]
+    public void TestCandidateOutsideConfiguration() {
+        var cfg = Messages.ConfigFromIds(A_ID, B_ID);
+        var log = new Log(new SnapshotDescriptor {
+            Idx = 0, Config = cfg
+        });
+        var fd = new DiscreteFailureDetector();
+        var a = CreateFollower(A_ID, log.Clone(), fd);
+        var b = CreateFollower(B_ID, log.Clone(), fd);
+        ElectionTimeout(a);
+        Communicate(a, b);
+        Assert.That(a.IsLeader, Is.True);
+        var newCfg = Messages.ConfigFromIds(B_ID);
+        a.AddEntry(newCfg);
+        Assert.That(!b.Log.GetConfiguration().IsJoint(), Is.True);
+        CommunicateUntil(() => !a.GetConfiguration().IsJoint() && b.GetConfiguration().IsJoint(), a, b);
+        Assert.That(a.GetConfiguration().IsJoint, Is.False);
+        Assert.That(b.Log.GetConfiguration().IsJoint, Is.True);
+        fd.MarkDead(B_ID);
+        ElectionTimeout(a);
+        Assert.That(a.IsLeader, Is.False);
+        fd.MarkAlive(B_ID);
+        ElectionTimeout(a);
+        Assert.That(a.IsCandidate, Is.True);
+        CommunicateUntil(() => a.IsLeader, a, b);
+        Assert.That(a.IsLeader, Is.True);
+        Communicate(a, b);
+        Assert.That(b.IsLeader, Is.True);
+    }
+}
