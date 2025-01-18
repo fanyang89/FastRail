8704e1713051d3e43471882c26ad64697e844d8a
fanyang <fanyang89@outlook.com>
2025-01-18 10:05:22 +0800

    Fix warning

--- RaftNET/FSM.Config.cs
+++ RaftNET/FSM.Config.cs
@@ -3,25 +3,21 @@
 namespace RaftNET;
 
 public partial class FSM {
-    public class Config(
-        bool enablePreVote,
-        int appendRequestThreshold,
-        int maxLogSize
-    ) : IDeepCloneable<Config> {
-        // If set to true will enable prevoting stage during election
+    public class Config(bool enablePreVote, int appendRequestThreshold, int maxLogSize) : IDeepCloneable<Config> {
+        // If set to true will enable pre-voting stage during election
         public bool EnablePreVote { get; set; } = enablePreVote;
 
         // max size of appended entries in bytes
         public int AppendRequestThreshold { get; set; } = appendRequestThreshold;
 
         // Limit in bytes on the size of the in-memory part of the log after which
-        // which requests are stopped until the log is shrunk by a is shrunk by a snapshot.
-        // Should be greater than than the sum of the following log entry sizes,
+        // requests are stopped until the log is shrunk by a is shrunk by a snapshot.
+        // Should be greater than the sum of the following log entry sizes,
         // otherwise the state machine will deadlock.
         public int MaxLogSize { get; set; } = maxLogSize;
 
         public Config Clone() {
-            return new Config(enablePreVote, appendRequestThreshold, maxLogSize);
+            return new Config(EnablePreVote, AppendRequestThreshold, MaxLogSize);
         }
     }
 }
